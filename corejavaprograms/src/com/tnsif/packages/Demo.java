c775bcc7ac7f29dfe545d377e62fadf334e52ca0
user <user@DESKTOP-VN77URQ>
2024-07-03 01:36:14 -0700

    commit messsage

---
+++ corejavaprograms/src/com/tnsif/packages/Demo.java
@@ -0,0 +1,9 @@
+package com.tnsif.packages;
+
+public class Demo {
+	public static void main(String[] args) {
+		Publicdemo p=new Publicdemo();
+		System.out.println(p.a);
+	}
+
+}
---
+++ corejavaprograms/src/com/tnsif/packages/Publicdemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.packages;
+
+import java.security.DomainCombiner;
+
+public class Publicdemo {
+	int a=80;
+	public static void main(String[] args) {
+		Publicdemo p=new Publicdemo();
+		System.out.println(p.a);
+	}
+
+}
